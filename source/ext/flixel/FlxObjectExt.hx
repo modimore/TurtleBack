2ff28e426a8d95ca6817caab5f18900575c104e3
modimore <quinn.e.mortimer@gmail.com>
2017-04-21 17:38:28 -0400

    Adds some offset helpers to the FlxObject extender.
    These allow the user to set the distance between one side of itself
    and the corresponding side of another FlxObject.
    Previously there was only a mechanism for aligning corresponding
    sides.

--- source/ext/flixel/FlxObjectExt.hx
+++ source/ext/flixel/FlxObjectExt.hx
@@ -63,4 +63,40 @@ class FlxObjectExt
 	{
 		objMobile.y = objFixed.y + (objFixed.height / 2) - (objMobile.height / 2);
 	}
+	/**
+	 * Offsets the left of one sprite from the left of another.
+	 * @param	objMobile	The FlxObject that will be moved.
+	 * @param	objFixed	The FlxObject serving as a position reference.
+	 */
+	static public function offsetFromLeft(objMobile:FlxObject, objFixed:FlxObject, offsetX:Float):Void
+	{
+		objMobile.x = objFixed.x + offsetX;
+	}
+	/**
+	 * Offsets the right of one sprite from the right of another.
+	 * @param	objMobile	The FlxObject that will be moved.
+	 * @param	objFixed	The FlxObject serving as a position reference.
+	 */
+	static public function offsetFromRight(objMobile:FlxObject, objFixed:FlxObject, offsetX:Float):Void
+	{
+		objMobile.x = objFixed.x + objFixed.width - objMobile.width + offsetX;
+	}
+	/**
+	 * Offsets the top of one sprite from the top of another.
+	 * @param	objMobile	The FlxObject that will be moved.
+	 * @param	objFixed	The FlxObject serving as a position reference.
+	 */
+	static public function offsetFromTop(objMobile:FlxObject, objFixed:FlxObject, offsetY:Float):Void
+	{
+		objMobile.y = objFixed.y + offsetY;
+	}
+	/**
+	 * Offsets the bottom of one sprite from the bottom of another.
+	 * @param	objMobile	The FlxObject that will be moved.
+	 * @param	objFixed	The FlxObject serving as a position reference.
+	 */
+	static public function offsetFromBottom(objMobile:FlxObject, objFixed:FlxObject, offsetY:Float):Void
+	{
+		objMobile.y = objFixed.y + objFixed.height - objMobile.height + offsetY;
+	}
 }
